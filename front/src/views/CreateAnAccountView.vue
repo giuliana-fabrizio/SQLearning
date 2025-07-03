b537f90be234ae4349edf4e57b7cd0cec7678aa7
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-07-04 00:09:55 +0200

    refactor: images

--- front/src/views/CreateAnAccountView.vue
+++ front/src/views/CreateAnAccountView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img alt="background" src="../assets/background.png"
+        <img alt="background" src="../../public/images/background.png"
             class="img-fluid position-fixed w-100 img-create-an-account">
 
         <div class="d-flex justify-content-center position-relative pt-4">
