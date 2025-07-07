6c6278ea1737d626048ed92527b52304748c98fd
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-07-07 21:16:12 +0200

    fix: database's query

--- back/queries/db.queries.js
+++ back/queries/db.queries.js
@@ -2,7 +2,7 @@ const getDatabases = "\
     select count(r.id_user) as peoples, u.avatar, d.*\
     from database as d\
     left join registers as r on r.id_database = d.id\
-    inner join user as u on u.id = r.id_user\
+    inner join user as u on u.id = d.id_user\
     group by d.id\
 ";
 
