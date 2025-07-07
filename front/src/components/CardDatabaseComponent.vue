a5339e48a7a6b9fe0920a4b0bb1583ddcc3239da
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-07-08 01:50:25 +0200

    feat: add dropdown to delete or update db

--- front/src/components/CardDatabaseComponent.vue
+++ front/src/components/CardDatabaseComponent.vue
@@ -6,29 +6,37 @@
             </div>
             <div class="col-8 col-md-9 col-lg-10">
                 <p class="fs-5 mb-1">{{ data.name }}</p>
-                <p
-                    class="mb-1"
-                    style="color: #49454F">
+                <p class="mb-1" style="color: #49454F">
                     {{ data.description.substring(0, 120) + "..." }}
                 </p>
             </div>
             <div class="align-items-start col-4 col-sm-1 d-flex justify-content-end">
-                <div
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    data-bs-title="Nombre d'inscrits"
-                    class="badge me-1 mt-1 ps-2 pe-2 rounded-circle"
-                    :style="{ background: purple.color_8 }">
+                <div data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Nombre d'inscrits"
+                    class="badge me-1 rounded-circle badge-peoples" :style="{ background: purple.color_8 }">
                     <span class="text-white" style="font-size: small;">{{ data.peoples }}</span>
                 </div>
-                <button class="btn p-0">
-                    <i class="bi bi-three-dots-vertical icon"></i>
-                </button>
+
+                <div v-if="current_user === data.id_user || role === 1" class="dropdown dropdown-plus">
+                    <a role="button" data-bs-toggle="dropdown" aria-expanded="false" class="dropdown-toggle-plus"
+                        :style="{ color: purple.color_11 }">
+                        <i class="bi bi-three-dots-vertical"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end  mt-2 p-0">
+                        <li>
+                            <button class="btn btn-sm dropdown-item text-success">
+                                <i class="bi bi-pencil-square me-2"></i>Éditer
+                            </button>
+                        </li>
+                        <li>
+                            <a role="button" class="btn btn-sm dropdown-item text-danger">
+                                <i class="bi bi-trash me-2"></i>Supprimer
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <div
-            class="border-2 border-bottom mx-auto pb-2"
-            style="border-color: #CAC4D0 !important; width: 95%">
+        <div class="border-2 border-bottom mx-auto pb-2" style="border-color: #CAC4D0 !important; width: 95%">
         </div>
     </div>
 </template>
@@ -40,6 +48,8 @@ export default {
     name: "CardDatabaseComponent",
 
     data: () => ({
+        current_user: localStorage.getItem("uid"),
+        role: localStorage.getItem("role"),
         purple
     }),
 
