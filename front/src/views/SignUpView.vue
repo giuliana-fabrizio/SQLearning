6d704e9aed7f6450b5f4ee2b198420b5777ff000
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-07-09 00:09:36 +0200

    feat: display alert

--- front/src/views/SignUpView.vue
+++ front/src/views/SignUpView.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <img alt="background" src="@/assets/background.png"
-            class="img-fluid position-fixed w-100 img-auth">
+        <img alt="background" src="@/assets/background.png" class="img-fluid position-fixed w-100 img-auth">
 
         <div class="d-flex justify-content-center position-relative pt-4">
             <div class="col-10 col-sm-8 col-md-6 col-lg-4 mt-5 mb-5">
+                <AlertComponent :alert_type="alert.type" :message="alert.message" :show="alert.show"
+                    @close="closeAlert" />
+
                 <h2 class="mb-5 text-center text-white">SQLEARNING</h2>
 
                 <div>
-                    <ProfileComponent :submit_message="message" @submit="submit" @reset_message="reset_message"
-                        form_class="bg-form-auth" input_class="bg-input-auth" />
+                    <ProfileComponent @alert="displayAlert" @submit="submit" form_class="bg-form-auth"
+                        input_class="bg-input-auth" />
 
                     <div class="card rounded-0 rounded-bottom pb-4 bg-form-auth">
                         <div class="d-flex align-items-center justify-content-center">
@@ -30,18 +32,25 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 
 import { purple } from "@/utils/colors";
+import AlertComponent from "@/components/AlertComponent.vue";
 import ProfileComponent from "@/components/ProfileComponent.vue";
 
 export default {
     name: "CreateAnAccountView",
 
     components: {
+        AlertComponent,
         ProfileComponent
     },
 
     data: () => ({
-        message: "",
         port: 0,
+
+        alert: {
+            type: "",
+            message: "",
+            show: false
+        },
 
         purple
     }),
@@ -53,7 +62,7 @@ export default {
     methods: {
         submit(user_to_create) {
             if (user_to_create.check_password != user_to_create.password) {
-                this.message = "Les mots de passe ne correspondent pas.";
+                this.updateAlert("alert-warning", "Les mots de passe ne correspondent pas.", true);
             } else {
                 createUserWithEmailAndPassword(auth, user_to_create.mail, user_to_create.password)
                     .then((credential) => {
@@ -63,17 +72,25 @@ export default {
                             })
                             .catch((error) => {
                                 error = error.response.data.data;
-                                this.message = error.message + " " + error.details;
+                                this.updateAlert("alert-danger", error.message + " " + error.details, true);
                             });
                     })
                     .catch((error) => {
-                        this.message = error.message;
+                        this.updateAlert("alert-danger", error.message, true);
                     });
             }
         },
 
-        reset_message() {
-            this.message = "";
+        closeAlert() { this.updateAlert("", "", false); },
+
+        displayAlert(alert) { this.alert = alert; },
+
+        updateAlert(type, message, show) {
+            this.alert = {
+                type: type,
+                message: message,
+                show: show
+            }
         }
     }
 }
