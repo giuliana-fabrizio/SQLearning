130b45354246302a15653926682b2ce68915c901
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-07-08 19:08:09 +0200

    feat: wip can update password in firebase

--- front/src/components/ProfileComponent.vue
+++ front/src/components/ProfileComponent.vue
@@ -1,25 +1,24 @@
 <template>
     <div>
-        <AlertComponent :alert_type="alert.type" :message="alert.message" :show="alert.show" @close="closeAlert" />
         <form @submit.prevent="submit()" class="card rounded-0 rounded-top p-4" :class="form_class">
             <fieldset>
                 <div class="row gx-3">
                     <div class="col-sm-6 mb-3">
                         <label for="firstname" class="fw-bolder ps-0">Prénom</label>
-                        <input id="firstname" type="text" v-model="user_property.firstname"
+                        <input id="firstname" type="text" v-model="user_values.firstname"
                             class="form-control text-center rounded-0" :class="input_class" required>
                     </div>
 
                     <div class="col-sm-6 mb-3">
                         <label for="name" class="fw-bolder ps-0">Nom</label>
-                        <input id="name" type="text" v-model="user_property.name"
+                        <input id="name" type="text" v-model="user_values.name"
                             class="form-control text-center rounded-0" :class="input_class" required>
                     </div>
                 </div>
 
                 <div class="mb-3">
                     <label for="field" class="fw-bolder ps-0">Secteur d'activité</label>
-                    <select id="field" v-model="user_property.id_field" class="form-control rounded-0"
+                    <select id="field" v-model="user_values.id_field" class="form-control rounded-0"
                         :class="input_class" required>
                         <option value="" disabled>Choisir...</option>
                         <option v-for="(field, index) in fields" :key="index" :value="field.id">
@@ -30,26 +29,29 @@
 
                 <div class="mb-3">
                     <label for="mail" class="fw-bolder ps-0">Email</label>
-                    <input id="mail" type="email" v-model="user_property.mail"
-                        class="form-control text-center rounded-0" :class="input_class" required :disabled="user">
+                    <input id="mail" type="email" v-model="user_values.mail" class="form-control text-center rounded-0"
+                        :class="input_class" required :disabled="current_user">
                 </div>
 
                 <div class="row gx-3">
                     <div class="col-sm-6 mb-3">
                         <label for="password" class="fw-bolder ps-0">Mot de passe</label>
                         <input @change="verifyPassword" id="password" type="password" minlength="8"
-                            v-model="user_property.password" class="form-control text-center rounded-0"
-                            :class="input_class" :required="!user">
+                            v-model="user_values.password" class="form-control text-center rounded-0"
+                            :class="input_class" :required="!current_user">
                     </div>
 
                     <div class="col-sm-6 mb-3">
+                        {{ user_values.password && user_values.password != '' }}
                         <label for="check_password" class="fw-bolder ps-0">Confirmer</label>
-                        <input id="check_password" type="password" minlength="8" v-model="user_property.check_password"
-                            class="form-control text-center rounded-0" :class="input_class" :required="!user">
+                        <input id="check_password" type="password" minlength="8" v-model="user_values.check_password"
+                            class="form-control text-center rounded-0" :class="input_class"
+                            :required="!current_user || user_values.password && user_values.password != ''">
                     </div>
                 </div>
 
-                <AvatarsComponent v-if="user" class="mb-3" :avatar="user_property.avatar" @resetAvatar="resetAvatar" />
+                <AvatarsComponent v-if="current_user" class="mb-3" :avatar="user_values.avatar"
+                    @resetAvatar="resetAvatar" />
 
                 <button type="submit" class="btn mt-3 text-white w-100" :style="{ background: purple.color_11 }">
                     Valider
@@ -63,14 +65,12 @@
 import axios from "axios";
 
 import { purple } from "@/utils/colors";
-import AlertComponent from "./AlertComponent.vue";
 import AvatarsComponent from "./AvatarsComponent.vue";
 
 export default {
     name: "ProfileComponent",
 
     components: {
-        AlertComponent,
         AvatarsComponent
     },
 
@@ -79,7 +79,7 @@ export default {
         port: 0,
         show_avatars: false,
 
-        user_property: {
+        user_values: {
             id: "",
             avatar: "",
             firstname: "",
@@ -90,19 +90,13 @@ export default {
             check_password: ""
         },
 
-        alert: {
-            type: "",
-            message: "",
-            show: false
-        },
         isUserGood: true,
 
         purple
     }),
 
     props: {
-        user: Object,
-        submit_message: String,
+        current_user: Object,
         form_class: String,
         input_class: String
     },
@@ -119,52 +113,35 @@ export default {
     },
 
     watch: {
-        user: function (user, _) {
-            this.user_property = { ...user };
-        },
-
-        submit_message: function (message, _) {
-            if (message != "") {
-                this.alert.type = message.includes("succès") ? "alert-success" : "alert-danger";
-                this.alert.message = message;
-                this.alert.show = true;
-            }
-        }
+        current_user: function (current_user, _) { this.user_values = { ...current_user }; },
     },
 
     methods: {
         submit() {
-            if (this.isUserGood) {
-                this.$emit('submit', this.user_property);
-                // document.getElementById("id_collapse_avatar")?.classList.remove("show"); // TODO fermer les avatars si on submit ?
-            } else {
-                this.alert.show = true;
-            }
+            if (this.isUserGood) { this.$emit('submit', this.user_values); }
+            else { this.alert.show = true; }
         },
 
         verifyPassword() {
             const regex = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#@$%&? "]).*$/;
 
-            if (!regex.test(this.user_property.password)) {
-                this.alert = {
+            if (this.user_values.password === '') {
+                this.isUserGood = true;
+            }
+            else if (!regex.test(this.user_values.password)) {
+                this.$emit("alert", {
                     type: "alert-warning",
                     message: "Le mot de passe doit contenir des caractères spéciaux (!#@$%&?), une majuscule, un nombre et doit faire minimum 8 caractères.",
                     show: true
-                }
+                });
                 this.isUserGood = false;
-            } else {
+            }
+            else {
                 this.isUserGood = true;
             }
         },
 
-        resetAvatar(avatar) {
-            this.user_property.avatar = avatar;
-        },
-
-        closeAlert() {
-            this.alert.show = false;
-            this.$emit("reset_message");
-        }
+        resetAvatar(avatar) { this.user_values.avatar = avatar; },
     }
 }
 </script>
--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -1,25 +1,49 @@
 <template>
     <div class="d-flex justify-content-center pt-4">
         <div class="col-10 col-sm-8 col-md-6 mt-5 mb-5">
+            <AlertComponent :alert_type="alert.type" :message="alert.message" :show="alert.show" @close="closeAlert" />
+
+            <Modal v-model="modal_psd.display" title="Confirmer votre mot de passe actuel" :modalStyle="modal_psd.style"
+                class="p-3">
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe</label>
+                    <input id="password" type="password" minlength="8" v-model="modal_psd.value" class="form-control">
+                </div>
+
+                <div class="d-flex justify-content-end">
+                    <button @click="cancelPassword" class="btn me-2 pe-4 ps-4"
+                        :style="{ background: purple.color_4 }">Annuler</button>
+                    <button @click="changePassword" class="btn pe-4 ps-4 text-white"
+                        :style="{ background: purple.color_10 }">Valider</button>
+                </div>
+            </Modal>
+
             <!-- <h2 class="fw-bold mb-5 text-center" :style="{ color: purple.color_11 }">Mon profil</h2> -->
             <img v-if="avatar" :src="avatar" class="mb-5 icon-avatar">
             <i v-else class="bi bi-person mb-5 icon-avatar"></i>
 
-            <ProfileComponent :user="current_user" :submit_message="message" @submit="submit"
-                @reset_message="reset_message" />
+            <ProfileComponent @alert="displayAlert" :current_user="current_user" @submit="submit" />
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { getAuth, updatePassword, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
+import VueModal from '@kouts/vue-modal';
+import '@kouts/vue-modal/dist/vue-modal.css';
+
 import { purple } from "@/utils/colors";
+import AlertComponent from "@/components/AlertComponent.vue";
 import ProfileComponent from "@/components/ProfileComponent.vue";
 
 export default {
     name: "ProfileView",
 
     components: {
+        AlertComponent,
+        'Modal': VueModal,
         ProfileComponent
     },
 
@@ -27,8 +51,23 @@ export default {
         port: 0,
 
         current_user: {},
+        user_values: {},
         avatar: null,
-        message: "",
+
+        alert: {
+            type: "",
+            message: "",
+            show: false
+        },
+
+        modal_psd: {
+            display: false,
+            value: "",
+            style: {
+                borderRadius: '0.25rem',
+                padding: '10px',
+            }
+        },
 
         purple
     }),
@@ -49,23 +88,72 @@ export default {
                         null;
                 })
                 .catch(_ => {
-                    this.message = 'Erreur inconnu'
+                    this.updateAlert("alert-danger", "Erreur inconnue.", true);
+                });
+        },
+
+        updateUser(user) {
+            axios.put(`http://localhost:${this.port}/user`, { user: user })
+                .then(response => {
+                    this.loadUser(user.id);
+                    this.updateAlert("alert-success", response.data.data, true);
+                })
+                .catch(_ => {
+                    this.updateAlert("alert-danger", "Erreur inconnue.", true);
                 });
         },
 
         submit(user) {
-            axios.put(`http://localhost:${this.port}/user`, { user: user })
-                .then(response => {
-                    this.loadUser(user.id);
-                    this.message = response.data.data;
-                })
-                .catch(_ => {
-                    this.message = 'Erreur inconnu'
-                });
+            if (!user.password) {
+                this.updateUser(user);
+                return;
+            } else if (user.check_password != user.password) {
+                this.updateAlert("alert-warning", "Les mots de passe ne correspondent pas.", true);
+            } else {
+                this.modal_psd.display = true;
+                this.user_values = user;
+            }
         },
 
-        reset_message() {
-            this.message = "";
+        async changePassword() {
+            if (!this.modal_psd.value) {
+                this.updateAlert("alert-warning", "Veuillez saisir votre mot de passe actuel.", true);
+                return;
+            }
+            this.modal_psd.display = false;
+
+            const auth = getAuth();
+            const firebase_user = auth.currentUser;
+            const credential = EmailAuthProvider.credential(this.current_user.mail, this.modal_psd.value);
+
+            try {
+                await reauthenticateWithCredential(firebase_user, credential);
+                await updatePassword(firebase_user, this.user_values.password);
+                this.updateUser(this.user_values);
+            } catch (error) {
+                console.error(`Error update password : ${error}`)
+                this.updateAlert("alert-danger", "Échec dans la modification des champs (problème rencontré au niveau du mot de passe).", true)
+            }
+        },
+
+        cancelPassword() {
+            this.modal_psd = {
+                display: false,
+                value: null,
+            }
+            this.updateUser(this.user_values);
+        },
+
+        closeAlert() { this.updateAlert("", "", false); },
+
+        displayAlert(alert) { this.alert = alert; },
+
+        updateAlert(type, message, show) {
+            this.alert = {
+                type: type,
+                message: message,
+                show: show
+            }
         }
     }
 }
